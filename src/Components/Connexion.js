26654ab8a442a262acfea75b8dce4bee98e76df7
Rakotondratsirahonina <rakoto.tsira@gmail.com>
2018-07-28 13:00:24 +0300

    test bootstrap

--- src/Components/Connexion.js
+++ src/Components/Connexion.js
@@ -1,5 +1,6 @@
 import React from 'react';
 
+
 export default class Connexion extends React.Component {
 
 goAcceuil = event => {
@@ -19,7 +20,7 @@ const password = this.passwordInput.value;
 render() {
 
 return (
-
+/*
 <div className="connexionBox" onSubmit={e=> this.goAcceuil(e)}>
 <form className="connexion">
 <input type ="text" placeholder ="Pseudo" required ref={input =>{this.pseudoInput= input}}/>
@@ -27,7 +28,39 @@ return (
 <button type ="submit"> Se connecter </button>
 </form>
 </div>
+*/  
 
+
+<div className="container">
+<div className="row">
+<div className="panel panel-default col-sm-6 col-md-4 col-md-offset-4">
+  <div className="panel-body">
+    <form  noValidate>
+      <div className="form-group">
+        <label className="label-control">Email</label>
+        <input
+          className="form-control"
+         />
+      </div>
+      <div className="form-group">
+        <label className="label-control">Password</label>
+        <input
+          type="password"
+          className="form-control"
+         />
+      </div>
+
+      <button type="submit" className="btn btn-primary" data-toggle="button" aria-pressed="false" autocomplete="off">
+        Se connecter
+      </button>
+      <button type="submit" className="btn btn-primary" data-toggle="button" aria-pressed="false" autocomplete="off">
+        S'inscrire      
+      </button>
+    </form>
+  </div>
+</div>
+</div>
+</div>
 
 	)
 
