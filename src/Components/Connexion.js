7f9686c53d96ba0aa0993d313108d763e03b2f92
Rakotondratsirahonina <rakoto.tsira@gmail.com>
2018-07-29 07:12:10 +0300

    add style to components Sign in

--- src/Components/Connexion.js
+++ src/Components/Connexion.js
@@ -6,8 +6,8 @@ export default class Connexion extends React.Component {
 goAcceuil = event => {
  event.preventDefault();
 //alert(this.pseudoInput.value)
-const pseudo = this.pseudoInput.value;
-const password = this.passwordInput.value;
+//const pseudo = this.pseudoInput.value;
+//const password = this.passwordInput.value;
  this.props.router.push('/acceuil/');
 
   this.props.router.push({
@@ -20,42 +20,48 @@ const password = this.passwordInput.value;
 render() {
 
 return (
-/*
-<div className="connexionBox" onSubmit={e=> this.goAcceuil(e)}>
-<form className="connexion">
-<input type ="text" placeholder ="Pseudo" required ref={input =>{this.pseudoInput= input}}/>
-<input type ="text" placeholder ="Password" required ref={input =>{this.passwordInput= input}}/>
-<button type ="submit"> Se connecter </button>
-</form>
-</div>
-*/  
-
-
 <div className="container">
 <div className="row">
 <div className="panel panel-default col-sm-6 col-md-4 col-md-offset-4">
+<h5>Connectez vous pour accéder à votre contenu</h5>
   <div className="panel-body">
     <form  noValidate>
-      <div className="form-group">
-        <label className="label-control">Email</label>
-        <input
+      <div className="form-group  has-feedback">
+        <input placeholder="Email"
           className="form-control"
          />
+         <span><i class="glyphicon glyphicon-envelope form-control-feedback"></i></span>
       </div>
-      <div className="form-group">
-        <label className="label-control">Password</label>
-        <input
+      <div className="form-group has-feedback">
+        <input placeholder="Password"
           type="password"
           className="form-control"
          />
+         <span class="glyphicon glyphicon-lock form-control-feedback"></span>
       </div>
-
-      <button type="submit" className="btn btn-primary" data-toggle="button" aria-pressed="false" autocomplete="off">
+      <div class="row row-action">
+      <div className="col-sm">
+          <div class="checkbox  bolo">
+                <input type="checkbox" class="form-check-input" id="souv" value=""/>
+                <label id="labcheck" for="souv">
+                Se souvenir de moi
+                </label>
+            </div>
+      <a href="#">Mot de passe oublié?</a>
+      </div>
+      <div className="col-sm">
+      <div class="row">
+      <button type="submit" id="log" className="btn btn-primary" data-toggle="button" aria-pressed="false" autocomplete="off"  onClick={e=> this.goAcceuil(e)}>
         Se connecter
       </button>
-      <button type="submit" className="btn btn-primary" data-toggle="button" aria-pressed="false" autocomplete="off">
-        S'inscrire      
+      </div>
+      <div class="row">
+      <button type="submit" id="log" className="btn btn-primary" data-toggle="button" aria-pressed="false" autocomplete="off">
+        S inscrire
       </button>
+      </div>
+      </div>
+      </div>
     </form>
   </div>
 </div>
@@ -67,4 +73,3 @@ return (
 	}
 
 }
-
